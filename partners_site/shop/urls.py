d74ce8319e25e9d22a7ca820ae09986c872285dd
Vuacheslav <soloomonslava1991@gmail.com>
2026-03-30 18:06:18 +0300

    .

--- partners_site/shop/urls.py
+++ partners_site/shop/urls.py
@@ -3,7 +3,7 @@ from shop import views
 
 urlpatterns = [
     path('', views.catalog_view, name='catalog'),
-    path('products/', views.catalog_view, name='catalog'),
+    path('products/', views.catalog_view, name='catalog_legacy'),
     path('product/group/<int:pk>/', views.product_group_detail, name='product_group_detail'),
     path('api/product/group/<int:pk>/', views.product_group_api, name='product_group_api')
 ]

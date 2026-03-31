831e6b25d3a18839b5fa6e96484d532483f4668a
Vuacheslav <soloomonslava1991@gmail.com>
2026-03-31 11:43:19 +0300

    .

--- partners_site/users/views.py
+++ partners_site/users/views.py
@@ -3,12 +3,13 @@ from django.contrib.auth import login as auth_login
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
+from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.views.decorators.http import require_http_methods
 
 from integrations.amocrm.exceptions import AmoCRMError, ContactCustomerBindingError
-from orders.models import Order
+from orders.models import Cart, Order
 from users.forms import CabinetCredentialsForm
 from users.models import User
 from users.services.amocrm_login import (
@@ -111,6 +112,34 @@ def user_cabinet_view(request):
         .prefetch_related("items", "items__product")
         .order_by("-time_created")
     )
+    latest_order = orders.first()
+
+    active_cart = (
+        Cart.objects
+        .filter(user=user, status=Cart.Status.ACTIVE)
+        .select_related("address", "requisites")
+        .prefetch_related("items")
+        .order_by("-time_updated")
+        .first()
+    )
+    active_cart_items_total = 0
+    if active_cart is not None:
+        active_cart_items_total = active_cart.items.aggregate(total_qty=Sum("qty")).get("total_qty") or 0
+
+    default_address = (
+        user.addresses
+        .filter(is_default=True)
+        .order_by("-time_updated")
+        .first()
+        or user.addresses.order_by("-time_updated").first()
+    )
+    default_requisites = (
+        user.requisites_set
+        .filter(is_default=True)
+        .order_by("-time_updated")
+        .first()
+        or user.requisites_set.order_by("-time_updated").first()
+    )
 
     return render(
         request,
@@ -119,5 +148,10 @@ def user_cabinet_view(request):
             "user": user,
             "orders": orders,
             "credentials_form": credentials_form,
+            "latest_order": latest_order,
+            "active_cart": active_cart,
+            "active_cart_items_total": active_cart_items_total,
+            "default_address": default_address,
+            "default_requisites": default_requisites,
         },
     )
